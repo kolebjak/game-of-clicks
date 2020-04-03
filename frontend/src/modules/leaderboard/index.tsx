6d5c21ae0e81d2c15e98546630c5f26a77fc7929
Jakub Kolebaba <jakub@kolebaba.cz>
2020-04-03 20:24:03 +0200

    Moved from react-scripts-ts to react-scripts

--- frontend/src/modules/leaderboard/index.tsx
+++ frontend/src/modules/leaderboard/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as classNames from 'classnames';
+import React from 'react';
+import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { FetchAction, fetchAction } from './actions';
 import { selectLeaderboard } from './reducer';
