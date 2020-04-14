a915aaaf9b6a6fe587ff0f00e033f61033dde74e
Jakub Kolebaba <jakub@kolebaba.cz>
2020-04-14 14:30:25 +0200

    Eslint for frontend

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,37 +1,40 @@
 import * as React from 'react';
+import { FormEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Leaderboard from './Leaderboard';
-import PageContainer from "./components/PageContainer";
-import Page from "./components/Page";
-import Button, {ButtonSize} from "./components/Button";
-import {FormEvent, useState} from "react";
-import {useHistory} from 'react-router-dom';
+import PageContainer from './components/PageContainer';
+import Page from './components/Page';
+import Button, { ButtonSize } from './components/Button';
 
 const HomePage: React.FC = () => {
-    const [teamName, setTeamName] = useState<string>();
-    const history = useHistory();
+  const [teamName, setTeamName] = useState<string>();
+  const history = useHistory();
 
-    const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-        setTeamName(e.currentTarget.value)
-    }
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setTeamName(e.currentTarget.value);
+  };
 
-    const onSubmit = (e: FormEvent) => {
-        e.preventDefault()
-        history.push(`/${teamName}`);
-    }
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    history.push(`/${teamName}`);
+  };
 
-    return (
-        <PageContainer>
-            <Page>
-                <form onSubmit={onSubmit}>
-                    <label htmlFor="teamNameInput">Enter your team name:</label><br/>
-                    <input id="teamNameInput" onChange={onChange} required={true}/>
-                    <Button size={ButtonSize.small} type="submit">click</Button>
-                </form>
-                <h2>Top 10 clickers</h2>
-                <Leaderboard/>
-            </Page>
-        </PageContainer>
-    );
-}
+  return (
+    <PageContainer>
+      <Page>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="teamNameInput">Enter your team name:</label>
+          <br />
+          <input id="teamNameInput" onChange={onChange} required />
+          <Button size={ButtonSize.small} type="submit">
+            click
+          </Button>
+        </form>
+        <h2>Top 10 clickers</h2>
+        <Leaderboard />
+      </Page>
+    </PageContainer>
+  );
+};
 
 export default HomePage;
---
+++ frontend/src/context.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const SessionContext = React.createContext<string>('');
+export default SessionContext;
