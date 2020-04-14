f00c00bad54103069afe7ae953f69c5ae4c5a177
Jakub Kolebaba <jakub@kolebaba.cz>
2020-04-14 13:37:35 +0200

    Limit leaderboard should display only 10 results (#22)

--- server/src/modules/leaderboard.js
+++ server/src/modules/leaderboard.js
@@ -4,5 +4,6 @@ export default async () =>
     Click.aggregate([
         {$group: {_id: '$team', id: {$first: '$team'}, team: {$first: '$team'}, teamCount: {$sum: 1}}},
         {$sort: {teamCount: -1}},
+        { $limit : 10 }
     ])
 
