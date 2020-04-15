d231ab4dadd8d6dd53b9d0cb4104ae779302f869
Jakub Kolebaba <jakub@kolebaba.cz>
2020-04-15 10:24:54 +0200

    Fix deployment authentication (#29)

--- _scripts/heroku_deploy.sh
+++ _scripts/heroku_deploy.sh
@@ -11,7 +11,7 @@ echo "Deploying Heroku Version $HEROKU_VERSION"
 
 URL_BLOB=`curl -s -n -X POST https://api.heroku.com/apps/$APP_NAME/sources \
 -H 'Accept: application/vnd.heroku+json; version=3' \
--H "Authorization: Bearer $secrets.access_github_email"`
+-H "Authorization: Bearer $HEROKU_API_KEY"`
 
 PUT_URL=`echo $URL_BLOB | python -c 'import sys, json; print(json.load(sys.stdin)["source_blob"]["put_url"])'`
 GET_URL=`echo $URL_BLOB | python -c 'import sys, json; print(json.load(sys.stdin)["source_blob"]["get_url"])'`
